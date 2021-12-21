d64b2197a3fb8c2da8a82851f25b335dfeb535be
rioanandaputra-id <rioanandaputra1998@gmail.com>
2021-12-21 10:03:23 +0700

    laravel v8.65, add modules, create module: Authentication, SdmTendik

---
+++ base_project/Modules/SdmTendik/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('sdmtendik::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('sdmtendik.name') !!}
+    </p>
+@endsection
