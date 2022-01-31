f41dc2676ab03dce72bcfe16f765179b777282e9
Mahendra Pratama <mpratama151291@gmail.com>
2022-01-31 12:02:10 +0700

    width percision

--- apps_pdpt/resources/views/dashboard/iku.blade.php
+++ apps_pdpt/resources/views/dashboard/iku.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <iframe title="pdut2 - IKU1" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYzAzNjg4MmEtZDYyYy00NTI0LWFmNDAtZTQzZTI1ZjI4NmExIiwidCI6IjMxY2U5YzFjLTg2ZTYtNGI2NC04NjI5LWQyMjM0Y2EyZjQ4YSIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+            <iframe title="pdut2 - IKU1" width="100%" height="720" src="https://app.powerbi.com/view?r=eyJrIjoiYzAzNjg4MmEtZDYyYy00NTI0LWFmNDAtZTQzZTI1ZjI4NmExIiwidCI6IjMxY2U5YzFjLTg2ZTYtNGI2NC04NjI5LWQyMjM0Y2EyZjQ4YSIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
         </div>
     </div>
 @endsection
