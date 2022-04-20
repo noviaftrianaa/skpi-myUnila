48994050b037c2affbb69d2acd7bd1862c825de3
Mahendra Pratama <mpratama151291@gmail.com>
2022-04-20 14:04:21 +0700

    update seeder

--- apps_pdpt/database/seeders/NeoFeederSeeder.php
+++ apps_pdpt/database/seeders/NeoFeederSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PDUT\Pdrd\Sms;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -39,57 +40,195 @@ class NeoFeederSeeder extends Seeder
             $token = $cari_token->token_value;
         }
 
+        $func = [
+//            'substansi_kuliah',
+            'akt_mhs'
+        ];
+
         // substansi kuliah
-        $get_data_substansi_kuliah = $this->curl_api_neo_feeder($url, $this->data_form('GetListSubstansiKuliah',$token));
-        $total_data_substansi_kuliah = count($get_data_substansi_kuliah);
-        if ($total_data_substansi_kuliah>0) {
-            foreach ($get_data_substansi_kuliah AS $no_substansi_kuliah=>$each_substansi_kuliah) {
-                echo 'Mendapatkan '.($no_substansi_kuliah+1).' dari '.$total_data_substansi_kuliah;
-                DB::table('pdrd.substansi_kuliah')->insert([
-                    'id_subst'	    => $each_substansi_kuliah['id_substansi'],
-//                    'id_sms'	    => $each_substansi_kuliah['id_prodi'],
-                    'id_jns_subst'	=> $each_substansi_kuliah['id_jenis_substansi'],
-                    'nm_subst'	    => $each_substansi_kuliah['nama_substansi'],
-                    'sks_mk'	    => $each_substansi_kuliah['sks_mata_kuliah'],
-                    'sks_tm'	    => $each_substansi_kuliah['sks_tatap_muka'],
-                    'sks_prak'	    => $each_substansi_kuliah['sks_praktek'],
-                    'sks_prak_lap'	=> $each_substansi_kuliah['sks_praktek_lapangan'],
-                    'sks_sim'	    => $each_substansi_kuliah['sks_simulasi'],
-                    'create_date'	=> date('Y-m-d H:i:s',strtotime($each_substansi_kuliah['tgl_create'])),
-                    'id_creator'	=> $id_creator,
-                    'last_update'	=> date('Y-m-d H:i:s',strtotime($each_substansi_kuliah['last_update'])),
-                    'id_updater'	=> $id_creator,
-                    'soft_delete'	=> 0,
-                    'last_sync'	    => currDateTime()
-                ]);
-                echo " (OK)\n";
+        if (in_array('substansi_kuliah',$func)) {
+            $get_data_substansi_kuliah = $this->curl_api_neo_feeder($url, $this->data_form('GetListSubstansiKuliah',$token));
+            $total_data_substansi_kuliah = count($get_data_substansi_kuliah);
+            if ($total_data_substansi_kuliah>0) {
+                foreach ($get_data_substansi_kuliah AS $no_substansi_kuliah=>$each_substansi_kuliah) {
+                    echo 'Mendapatkan '.($no_substansi_kuliah+1).' dari '.$total_data_substansi_kuliah;
+                    DB::table('pdrd.substansi_kuliah')->insert([
+                        'id_subst'	    => $each_substansi_kuliah['id_substansi'],
+                        'id_sms'	    => $each_substansi_kuliah['id_prodi'],
+                        'id_jns_subst'	=> $each_substansi_kuliah['id_jenis_substansi'],
+                        'nm_subst'	    => $each_substansi_kuliah['nama_substansi'],
+                        'sks_mk'	    => $each_substansi_kuliah['sks_mata_kuliah'],
+                        'sks_tm'	    => $each_substansi_kuliah['sks_tatap_muka'],
+                        'sks_prak'	    => $each_substansi_kuliah['sks_praktek'],
+                        'sks_prak_lap'	=> $each_substansi_kuliah['sks_praktek_lapangan'],
+                        'sks_sim'	    => $each_substansi_kuliah['sks_simulasi'],
+                        'create_date'	=> date('Y-m-d H:i:s',strtotime($each_substansi_kuliah['tgl_create'])),
+                        'id_creator'	=> $id_creator,
+                        'last_update'	=> date('Y-m-d H:i:s',strtotime($each_substansi_kuliah['last_update'])),
+                        'id_updater'	=> $id_creator,
+                        'soft_delete'	=> 0,
+                        'last_sync'	    => currDateTime()
+                    ]);
+                    echo " (OK)\n";
+                }
+            }
+        }
+
+        // aktivitas mhs
+        if (in_array('akt_mhs',$func)) {
+            $get_data_akt_mhs = $this->curl_api_neo_feeder($url, $this->data_form('GetListAktivitasMahasiswa', $token));
+            $total_data_akt_mhs = count($get_data_akt_mhs);
+            if ($total_data_akt_mhs > 0) {
+                foreach ($get_data_akt_mhs as $no_akt_mhs => $each_akt_mhs) {
+                    echo 'Mendapatkan ' . ($no_akt_mhs + 1) . ' dari ' . $total_data_akt_mhs;
+                    $cek_sms = Sms::find($each_akt_mhs['id_prodi']);
+                    if (!is_null($cek_sms)) {
+                        $cek_akt = DB::table('pdrd.akt_mhs')->where('id_akt_mhs',$each_akt_mhs['id_aktivitas'])->first();
+                        if (is_null($cek_akt)) {
+                            DB::table('pdrd.akt_mhs')->insert([
+                                'id_akt_mhs'    => $each_akt_mhs['id_aktivitas'],
+                                'id_jns_akt_mhs'=> $each_akt_mhs['id_jenis_aktivitas'],
+                                'id_sms'        => $each_akt_mhs['id_prodi'],
+                                'id_smt'        => $each_akt_mhs['id_semester'],
+                                'judul_akt_mhs' => $each_akt_mhs['judul'],
+                                'lokasi_kegiatan' => $each_akt_mhs['lokasi'],
+                                'sk_tugas'      => $each_akt_mhs['sk_tugas'],
+                                'tgl_sk_tugas'  => $each_akt_mhs['tanggal_sk_tugas'],
+                                'ket_akt'       => $each_akt_mhs['keterangan'],
+                                'a_komunal'     => $each_akt_mhs['untuk_kampus_merdeka'],
+                                'create_date'   => currDateTime(),
+                                'id_creator'    => $id_creator,
+                                'last_update'   => currDateTime(),
+                                'id_updater'    => $id_creator,
+                                'soft_delete'   => 0,
+                                'last_sync'     => currDateTime()
+                            ]);
+                            echo " (OK)\n";
+                        } else {
+                            echo " (SUDAH ADA)\n";
+                        }
+
+                        $get_data_ang_akt_mhs = $this->curl_api_neo_feeder($url, $this->data_form('GetListAnggotaAktivitasMahasiswa', $token,'id_aktivitas',$each_akt_mhs['id_aktivitas']));
+                        $total_data_ang_akt_mhs = count($get_data_ang_akt_mhs);
+                        if ($total_data_ang_akt_mhs > 0) {
+                            foreach ($get_data_ang_akt_mhs AS $no_ang_akt_mhs => $each_data_ang_akt_mhs) {
+                                $cek_ang_akt = DB::table('pdrd.anggota_akt_mhs')->where('id_ang_akt_mhs',$each_data_ang_akt_mhs['id_anggota'])->first();
+                                if (is_null($cek_ang_akt)) {
+                                    DB::table('pdrd.anggota_akt_mhs')->insert([
+                                        'id_ang_akt_mhs'=> $each_data_ang_akt_mhs['id_anggota'],
+                                        'id_akt_mhs'    => $each_data_ang_akt_mhs['id_aktivitas'],
+                                        'id_reg_pd'     => $each_data_ang_akt_mhs['id_registrasi_mahasiswa'],
+                                        'nm_pd'         => $each_data_ang_akt_mhs['nama_mahasiswa'],
+                                        'nipd'          => $each_data_ang_akt_mhs['nim'],
+                                        'jns_peran_mhs' => $each_data_ang_akt_mhs['jenis_peran'],
+                                        'create_date'   => currDateTime(),
+                                        'id_creator'    => $id_creator,
+                                        'last_update'   => currDateTime(),
+                                        'id_updater'    => $id_creator,
+                                        'soft_delete'   => 0,
+                                        'last_sync'     => currDateTime()
+                                    ]);
+                                }
+                            }
+                        }
+
+                        $get_data_konversi_mbkm = $this->curl_api_neo_feeder($url, $this->data_form('GetListKonversiKampusMerdeka', $token,'id_aktivitas',$each_akt_mhs['id_aktivitas']));
+                        $total_data_konversi_mbkm = count($get_data_konversi_mbkm);
+                        if ($total_data_konversi_mbkm > 0) {
+                            foreach ($get_data_konversi_mbkm AS $no_konversi_mbkm => $each_data_konversi_mbkm) {
+                                $cek_konversi = DB::table('mbkm.konversi_kampus_merdeka')->where('id_konversi_aktivitas',$each_data_konversi_mbkm['id_konversi_aktivitas'])->first();
+                                if (is_null($cek_konversi)) {
+                                    DB::table('mbkm.konversi_kampus_merdeka')->insert([
+                                        'id_konversi_aktivitas'=> $each_data_konversi_mbkm['id_konversi_aktivitas'],
+                                        'id_mk'         => $each_data_konversi_mbkm['id_matkul'],
+                                        'id_ang_akt_mhs'=> $each_data_konversi_mbkm['id_anggota'],
+                                        'id_akt_mhs'    => $each_data_konversi_mbkm['id_aktivitas'],
+//                                        'id_daftar_kampus_merdeka'          => $each_data_konversi_mbkm['nim'],
+                                        'nilai_angka'   => $each_data_konversi_mbkm['nilai_angka'],
+                                        'nilai_huruf'   => $each_data_konversi_mbkm['nilai_huruf'],
+                                        'nilai_indeks'  => $each_data_konversi_mbkm['nilai_indeks'],
+                                        'sks_mk'        => $each_data_konversi_mbkm['sks_mata_kuliah'],
+                                        'create_date'   => currDateTime(),
+                                        'id_creator'    => $id_creator,
+                                        'last_update'   => currDateTime(),
+                                        'id_updater'    => $id_creator,
+                                        'soft_delete'   => 0,
+                                        'last_sync'     => currDateTime()
+                                    ]);
+                                }
+                            }
+                        }
+                    } else {
+                        echo " (LEWATI)\n";
+                    }
+                }
+            }
+        }
+
+        // list konversi
+        if (in_array('konversi',$func)) {
+            $get_data_akt_mhs = $this->curl_api_neo_feeder($url, $this->data_form('GetListAktivitasMahasiswa', $token));
+            $total_data_akt_mhs = count($get_data_akt_mhs);
+            if ($total_data_akt_mhs > 0) {
+                foreach ($get_data_akt_mhs as $no_akt_mhs => $each_akt_mhs) {
+                    echo 'Mendapatkan ' . ($no_akt_mhs + 1) . ' dari ' . $total_data_akt_mhs;
+                    $cek_akt = DB::table('pdrd.akt_mhs')->where('id_akt_mhs',$each_akt_mhs['id_aktivitas'])->first();
+                    if (is_null($cek_akt)) {
+                        DB::table('pdrd.akt_mhs')->insert([
+                            'id_akt_mhs'    => $each_akt_mhs['id_aktivitas'],
+                            'id_jns_akt_mhs'=> $each_akt_mhs['id_jenis_aktivitas'],
+                            'id_sms'        => $each_akt_mhs['id_prodi'],
+                            'id_smt'        => $each_akt_mhs['id_semester'],
+                            'judul_akt_mhs' => $each_akt_mhs['judul'],
+                            'lokasi_kegiatan' => $each_akt_mhs['lokasi'],
+                            'sk_tugas'      => $each_akt_mhs['sk_tugas'],
+                            'tgl_sk_tugas'  => $each_akt_mhs['tanggal_sk_tugas'],
+                            'ket_akt'       => $each_akt_mhs['keterangan'],
+                            'a_komunal'     => $each_akt_mhs['untuk_kampus_merdeka'],
+                            'create_date'   => date('Y-m-d H:i:s', strtotime($each_akt_mhs['tgl_create'])),
+                            'id_creator'    => $id_creator,
+                            'last_update'   => date('Y-m-d H:i:s', strtotime($each_akt_mhs['last_update'])),
+                            'id_updater'    => $id_creator,
+                            'soft_delete'   => 0,
+                            'last_sync'     => currDateTime()
+                        ]);
+                        echo " (OK)\n";
+                    } else {
+                        echo " (SUDAH ADA)\n";
+                    }
+                }
             }
         }
 
         // Kurikulum
-        $get_data_substansi_kuliah = $this->curl_api_neo_feeder($url, $this->data_form('GetListSubstansiKuliah',$token));
-        $total_data_substansi_kuliah = count($get_data_substansi_kuliah);
-        if ($total_data_substansi_kuliah>0) {
-            foreach ($get_data_substansi_kuliah AS $no_substansi_kuliah=>$each_substansi_kuliah) {
-                echo 'Mendapatkan '.($no_substansi_kuliah+1).' dari '.$total_data_substansi_kuliah;
-                DB::table('pdrd.substansi_kuliah')->insert([
-                    'id_subst'	    => $each_substansi_kuliah['id_substansi'],
-//                    'id_sms'	    => $each_substansi_kuliah['id_prodi'],
-                    'id_jns_subst'	=> $each_substansi_kuliah['id_jenis_substansi'],
-                    'nm_subst'	    => $each_substansi_kuliah['nama_substansi'],
-                    'sks_mk'	    => $each_substansi_kuliah['sks_mata_kuliah'],
-                    'sks_tm'	    => $each_substansi_kuliah['sks_tatap_muka'],
-                    'sks_prak'	    => $each_substansi_kuliah['sks_praktek'],
-                    'sks_prak_lap'	=> $each_substansi_kuliah['sks_praktek_lapangan'],
-                    'sks_sim'	    => $each_substansi_kuliah['sks_simulasi'],
-                    'create_date'	=> date('Y-m-d H:i:s',strtotime($each_substansi_kuliah['tgl_create'])),
-                    'id_creator'	=> $id_creator,
-                    'last_update'	=> date('Y-m-d H:i:s',strtotime($each_substansi_kuliah['last_update'])),
-                    'id_updater'	=> $id_creator,
-                    'soft_delete'	=> 0,
-                    'last_sync'	    => currDateTime()
-                ]);
-                echo " (OK)\n";
+        if (in_array($func,'kurikulum')) {
+            $get_data_kurikulum_sp = $this->curl_api_neo_feeder($url, $this->data_form('GetListKurikulum',$token));
+            $total_data_kurikulum_sp = count($get_data_kurikulum_sp);
+            if ($total_data_kurikulum_sp>0) {
+                foreach ($get_data_kurikulum_sp AS $no_kurikulum_sp=>$each_kurikulum_sp) {
+                    echo 'Mendapatkan '.($no_kurikulum_sp+1).' dari '.$total_data_kurikulum_sp;
+                    DB::table('pdrd.kurikulum_sp')->insert([
+                        'id_kurikulum_sp'   => $each_kurikulum_sp['id_kurikulum'],
+                        'id_smt'            => $each_kurikulum_sp['id_semester'],
+                        'id_jenj_didik'     => $each_kurikulum_sp['id_jenj_didik'],
+                        'id_sms'	        => $each_kurikulum_sp['id_prodi'],
+                        'nm_kurikulum_sp'   => $each_kurikulum_sp['nama_kurikulum'],
+                        'jmlh_smt_normal'	=> $each_kurikulum_sp['jml_sem_normal'],
+                        'a_digunakan'	    => $each_kurikulum_sp['jml_sem_normal'],
+                        'jmlh_sks_lulus'	=> $each_kurikulum_sp['jumlah_sks_lulus'],
+                        'jmlh_sks_wajib'	=> $each_kurikulum_sp['jumlah_sks_wajib'],
+                        'jmlh_sks_pilihan'	=> $each_kurikulum_sp['jumlah_sks_pilihan'],
+                        'jmlh_sks_mk_wajib'	=> $each_kurikulum_sp['jumlah_sks_mata_kuliah_wajib'],
+                        'jmlh_sks_mk_pilih'	=> $each_kurikulum_sp['jumlah_sks_mata_kuliah_pilihan'],
+                        'create_date'	=> date('Y-m-d H:i:s',strtotime($each_kurikulum_sp['tgl_create'])),
+                        'id_creator'	=> $id_creator,
+                        'last_update'	=> date('Y-m-d H:i:s',strtotime($each_kurikulum_sp['last_update'])),
+                        'id_updater'	=> $id_creator,
+                        'soft_delete'	=> 0,
+                        'last_sync'	    => currDateTime()
+                    ]);
+                    echo " (OK)\n";
+                }
             }
         }
     }
