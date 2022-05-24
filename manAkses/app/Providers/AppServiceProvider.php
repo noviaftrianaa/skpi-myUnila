5677634344c7f1a7058fd44e6914540b201a63e5
muhammadikhsan94 <muhammadikhsan208@gmail.com>
2022-05-24 15:45:14 +0700

    update sidebar

--- manAkses/app/Providers/AppServiceProvider.php
+++ manAkses/app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use DB;
+use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +28,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $response = Http::get('http://onedata.unila.ac.id/api/live/0.1/man_akses/peran?id_pengguna=12B25CAC-0482-4BE7-9924-C61E364006DD');
+        $message = $response['message'];
+
+        if(!empty($message)) {
+            foreach($response['data'] AS $each_data) {
+                // dd($each_data);
+                View::share('users', $each_data);
+            }
+        }
     }
 }
--- manAkses/resources/views/template/default/sidebar.blade.php
+++ manAkses/resources/views/template/default/sidebar.blade.php
@@ -1,5 +1,26 @@
 <!-- Sidebar -->
 <div class="sidebar">
+	<!-- Profile Menu -->
+    <div class="user-panel mt-3 pb-2 mb-2 d-flex text-light">
+        <div class="image mt-1">
+            <img src="{{ asset('images/blank-profile.png') }}" class="mt-2"
+                alt="User Image">
+        </div>
+        <div class="info">
+            <a href="{{ url('/biodata') }}" class="d-block">{{ $users['nm_pengguna'] }}</a>
+            <span class="d-block text-sm">
+				<form action="{{ url('changeRole') }}" method="post" enctype="multipart/form-data" id="changeRole">
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+					<input type="hidden" name="_method" value="PUT">
+					<select class="form-control-plaintext bg-dark text-light peran" name="id_peran">
+						@foreach($users['status_peran'] AS $items)
+						<option value="{{ $items['id_peran'] }}" {{ ($items['id_peran']==session()->get('login.role')->id_peran) ? 'selected' : '' }}>{{ $items['nm_peran'] }}</option>
+						@endforeach
+					</select>
+				</form>
+			</span>
+        </div>
+    </div>
 	<!-- Sidebar Menu -->
 	<nav class="mt-2">
 		<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -10,6 +31,7 @@
 						<p>Dashboard</p>
 				</a>
 			</li>
+			<li class="nav-header">Master</li>
 			<li class="nav-item">
 			<!-- route dashboard -->
 				<a href="{{ route('user.index') }}" class="nav-link {{ (request()->is('user*')) ? 'active' : '' }}">
@@ -57,3 +79,17 @@
 	<!-- /.sidebar-menu -->
 </div>
 <!-- /.sidebar -->
+
+@push('js')
+<script>
+	$(document).ready(function() {
+		$('.peran').on('change', function() {
+			var parentForm = $(this).closest("form");
+			if (parentForm && parentForm.length > 0) {
+				parentForm.submit();
+			}
+			// document.forms['#changeRole'].submit();
+		});
+	});
+</script>
+@endpush
