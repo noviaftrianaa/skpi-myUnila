ff9ff35bf6db9986cd514d257c0466d4c2dd3d09
muhammadikhsan94 <muhammadikhsan208@gmail.com>
2022-05-30 14:01:46 +0700

    fix template

--- manAkses/resources/views/template/default/app.blade.php
+++ manAkses/resources/views/template/default/app.blade.php
@@ -54,7 +54,7 @@
         }   
     </style>
 </head>
-<body class="hold-transition sidebar-mini layout-fixed control-sidebar-slide-open text-sm">
+<body class="hold-transition sidebar-mini sidebar-collapse">
 <div class="wrapper">
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-info navbar-dark border-bottom-0 text-white">
