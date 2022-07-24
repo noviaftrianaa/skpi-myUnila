2e7c11e06c2d2eaa8df02669c73bc7c6fc2c2037
Mahendra Pratama <mpratama151291@gmail.com>
2022-07-24 07:10:00 +0700

    dashboard mhs

--- apps_pdpt/resources/views/dashboard/mahasiswa.blade.php
+++ apps_pdpt/resources/views/dashboard/mahasiswa.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <iframe title="{{ $data_1->kode_dashboard }}" width="100%" height="720" src="{{ $data_1->link_dashboard_bi }}" frameborder="0" allowFullScreen="true"></iframe>
+{{--            <iframe title="{{ $data_1->kode_dashboard }}" width="100%" height="720" src="{{ $data_1->link_dashboard_bi }}" frameborder="0" allowFullScreen="true"></iframe>--}}
         </div>
         <div class="row">
             <iframe title="{{ $data_2->kode_dashboard }}" width="100%" height="720" src="{{ $data_2->link_dashboard_bi }}" frameborder="0" allowFullScreen="true"></iframe>
