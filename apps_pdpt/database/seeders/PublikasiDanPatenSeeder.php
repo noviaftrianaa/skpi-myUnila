139d5ae7b2ec2dc77a1575c3df0296e91d7d7f8d
UNILABITBUCKET <lca@adm.unila.ac.id>
2022-11-14 00:47:32 +0700

    publikasi dan paten draft

--- apps_pdpt/database/seeders/PublikasiDanPatenSeeder.php
+++ apps_pdpt/database/seeders/PublikasiDanPatenSeeder.php
@@ -22,6 +22,7 @@ class PublikasiDanPatenSeeder extends Seeder
      */
     public function run()
     {
+        $sdm_collect_id = Sdm::where('soft_delete',0)->pluck('id_sdm')->toArray();
         $data_dosen_sister = \DB::connection('pgsql_sister')->SELECT("
             SELECT tsdm.id_sdm FROM pdrd.sdm AS tsdm
             JOIN pdrd.reg_ptk AS tr ON tr.id_sdm=tsdm.id_sdm AND tr.soft_delete=0
@@ -82,8 +83,83 @@ class PublikasiDanPatenSeeder extends Seeder
            }
        }
 
+       //litabmas
+       $cari_litabmas = \DB::connection('pgsql_sister')->table('pdrd.akt_ajar_dosen')
+        ->select([
+            'id_litabmas',
+            'id_lemb_iptek',
+            'judul_litabmas',
+            'lama_kegiatan',
+            'thn_laks_ke',
+            'dana_dikti',
+            'dana_pt',
+            'dana_institusi_lain',
+            'in_kind',
+            'stat_aktif',
+            'jns_litabmas',
+            'sk_tugas',
+            'tgl_sk_tugas',
+            'lokasi_kegiatan',
+            'id_skim',
+            'id_thn_usulan',
+            'id_thn_kegiatan',
+            'id_thn_laks',
+            'id_lanjutan_litabmas',
+            'id_kel_bidang',
+            'id_tse',
+            'id_smi',
+            'id_jns_lit',
+            'create_date',
+            'id_creator',
+            'last_update',
+            'id_updater',
+            'soft_delete',
+            'last_sync'
+        ])->get();
+        $total_litabmas = count($cari_litabmas);
+        foreach ($cari_litabmas AS $urutan_litabmas=>$each_litabmas_sister) {
+            echo "Litabmas ".($urutan_litabmas+1)." dari ".$total_litabmas." id:".$each_litabmas_sister->id_litabmas;
+            $cari_sdm_anggota_litabmas_sister = \DB::connection('pgsql_sister')->table('pdrd.sdm_anggota_litabmas')
+                ->select([
+                    'id_litabmas',
+                    'id_sdm',
+                    'id_katgiat',
+                    'peran_litabmas',
+                    'stat_aktif',
+                    'create_date',
+                    'id_creator',
+                    'last_update',
+                    'id_updater',
+                    'soft_delete',
+                    'last_sync'
+                ])
+                ->where('id_litabmas',$each_litabmas_sister->id_publikasi)
+                ->whereIn('id_sdm',$sdm_collect_id)->get();
+            if (count($cari_penulis_sister)>0) {
+                $cari_pub_od = Publikasi::find($each_pub_sister->id_publikasi);
+                if (is_null($cari_pub_od)) {
+                    $input_pub = (array) $each_pub_sister;
+                    $input_pub['tgl_terbit']    = date('Y-m-d',strtotime($each_pub_sister['tgl_terbit']));
+                    if (!in_array($input_pub['soft_delete'],[0,1])) {
+                        $input_pub['stat_impor_sinta']  = $input_pub['soft_delete'];
+                        $input_pub['soft_delete']       = (in_array($input_pub['soft_delete'],[4,5])?1:0);
+                    }
+                    $publikasi = new Publikasi();
+                    $publikasi->fill($input_pub)->save();
+                }
+                foreach ($cari_penulis_sister AS $each_penulis) {
+                    $cari_tulis_od = TulisPub::find($each_penulis->id_tulis_pub);
+                    if (is_null($cari_tulis_od)) {
+                        $input_tulis = (array) $each_penulis;
+                        $tulis_pub = new TulisPub();
+                        $tulis_pub->fill($input_tulis)->save();
+                    }
+                }
+            }
+            echo " (OK)\n";
+        }
+
         // publikasi
-        $sdm_collect_id = Sdm::where('soft_delete',0)->pluck('id_sdm')->toArray();
         $litabmas = Litabmas::where('soft_delete',0)->pluck('id_litabmas')->toArray();
         $cari_pub = \DB::connection('pgsql_sister')->table('pdrd.publikasi')
             ->select([
