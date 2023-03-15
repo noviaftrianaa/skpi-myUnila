054beaa506e102b07621a431d853936f5448b06d
muhammadikhsan94 <muhammadikhsan208@gmail.com>
2023-03-15 11:23:50 +0700

    update menu role manakses

--- apps_pdpt/config/database.php
+++ apps_pdpt/config/database.php
@@ -119,6 +119,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'trust_server_certificate' => true,
         ],
 
         'sqlsrv_live' => [
