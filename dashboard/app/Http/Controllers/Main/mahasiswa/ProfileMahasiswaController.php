3c0a321fdb46f8cf42d8044a17c7f34706542859
aryadzar <112409058+aryadzar@users.noreply.github.com>
2024-12-30 15:48:56 +0700

    Menyesuaikan database part 1

--- dashboard/app/Http/Controllers/Main/mahasiswa/ProfileMahasiswaController.php
+++ dashboard/app/Http/Controllers/Main/mahasiswa/ProfileMahasiswaController.php
@@ -11,6 +11,24 @@ class ProfileMahasiswaController extends Controller
     public function index(){
         $judul = "Halaman Profile";
 
-        return view($this->path_view.'profile_mhs.index', compact('judul'));
+        $id_pd_auth = \Auth::user()->id_pd_pengguna;
+
+        $q = "
+            SELECT *
+            FROM pdrd.peserta_didik WITH ( NOLOCK )
+            JOIN pdrd.reg_pd WITH ( NOLOCK ) ON reg_pd.id_pd = peserta_didik.id_pd
+            JOIN pdrd.sms WITH ( NOLOCK ) ON sms.id_sms = reg_pd.id_sms
+            AND sms.soft_delete = 0
+            WHERE peserta_didik.id_pd = ?
+        ";
+
+        $profile = \DB::selectOne($q, [$id_pd_auth]);
+
+        // dd($profile);
+
+        return view($this->path_view.'profile_mhs.index', compact(
+            'judul',
+            'profile'
+        ));
     }
 }
